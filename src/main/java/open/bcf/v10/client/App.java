004ef939b94e6979a32da1bb93156a95a2b1c4ee
Yoram Kulbak <ykulbak@aconex.com>
2015-12-08 00:33:40 +1100

    After running the maven archetype plugin

---
+++ src/main/java/open/bcf/v10/client/App.java
@@ -0,0 +1,13 @@
+package open.bcf.v10.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
